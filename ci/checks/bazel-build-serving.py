5b89987abea1119e0d9d36fb1d62e2a3fe6ad821
Zhao Lufan <zhao.lufan30@zte.com.cn>
2019-09-26 16:12:59 +0800

    Publish build artifacts

--- ci/checks/bazel-build-serving.py
+++ ci/checks/bazel-build-serving.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
+import os
+import shutil
 import subprocess
 import sys
 
 
 def main(args):
     subprocess.check_call(['bazel', 'build', *args, '//adlik_serving'])
+
+    os.makedirs('__adlik_serving', exist_ok=True)
+
+    shutil.copy2('bazel-bin/adlik_serving/adlik_serving', '__adlik_serving/adlik_serving')
 
 
 if __name__ == "__main__":
