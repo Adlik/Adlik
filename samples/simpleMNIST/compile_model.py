883322171f873f864a952f560a11d26e262a94fb
jinshiying <jin.shiying@zte.com.cn>
2019-11-11 11:26:59 +0800

    Add a MNIST sample to demonstrate how to use Adlik

--- samples/simpleMNIST/compile_model.py
+++ samples/simpleMNIST/compile_model.py
@@ -2,28 +2,21 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """
-This is a mnist sample which can compile keras h5 model to tf serving/openvino/tensorrt model
+This is a mnist sample which compile keras h5 model to tf serving/openvino/tensorrt model
 """
-import json
 import os
 
-from model_compiler import log_util
-from model_compiler.config import create_config
+from model_compiler.config import create_config_from_obj
 from model_compiler.runtime import create_compiler
-
-_LOGGER = log_util.get_logger(__file__)
 
 
 def _main():
-    log_util.setup_logger()
     base_dir = os.path.dirname(__file__)
     request = {
-        "serving_type": "tf",  # or openvino/tensorrt
-        "h5_path": os.path.join(base_dir, "./model.h5"),
-        "script_path": os.path.join(base_dir, "model.py"),
-        "checkpoint_path": "",
-        "frozen_graph_path": "",
-        "export_path": os.path.join(base_dir, "serving_model"),
+        "serving_type": "tf",  # or openvino, tensorrt
+        "h5_path": os.path.join(base_dir, "./model/mnist.h5"),
+        "script_path": os.path.join(base_dir, "train_model.py"),
+        "export_path": os.path.join(base_dir, "model_repos"),
         "input_layer_names": [
             "conv2d_1"
         ],
@@ -45,16 +38,14 @@ def _main():
         "output_signatures": [
             "label"
         ],
-        "model_name": "test",
+        "model_name": "mnist",
         "job_id": "mnist",
-        "callback": "",
         "max_batch_size": 128
     }
 
-    _LOGGER.info('Prepare run test, request: %s', request)
-    compiler = create_compiler(create_config(from_source='json', json_message=json.dumps(request)))
+    compiler = create_compiler(create_config_from_obj(request))
     result = compiler.compile()
-    _LOGGER.info('result: %s', result)
+    print(result)
 
 
 if __name__ == "__main__":
