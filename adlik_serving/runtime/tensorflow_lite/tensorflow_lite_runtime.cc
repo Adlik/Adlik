f7c3b87c232b8de5a3001684b9acec7cd6d2e287
Zhao Lufan <zhao.lufan30@zte.com.cn>
2020-02-12 15:26:56 +0800

    Add a placeholder runtime
    Signed-off-by: Zhao Lufan <zhao.lufan30@zte.com.cn>

--- adlik_serving/runtime/tensorflow_lite/tensorflow_lite_runtime.cc
+++ adlik_serving/runtime/tensorflow_lite/tensorflow_lite_runtime.cc
@@ -1,2 +1,25 @@
 // Copyright 2019 ZTE corporation. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
+
+#include "adlik_serving/runtime/batching/batching_model.h"
+#include "adlik_serving/runtime/batching/composite_batch_processor.h"
+#include "adlik_serving/runtime/util/unique_batcher_runtime_helper.h"
+
+namespace {
+
+using adlik::serving::BatchingModel;
+using adlik::serving::CompositeBatchProcessor;
+using adlik::serving::ModelConfig;
+using adlik::serving::ModelId;
+
+class TensorFlowLiteModel : public CompositeBatchProcessor, public BatchingModel {
+public:
+  static cub::Status create(const ModelConfig& modelConfig,
+                            const ModelId& modelId,
+                            std::unique_ptr<TensorFlowLiteModel>* model) {
+    throw std::logic_error("TODO");
+  }
+};
+
+DEFINE_UNIQUE_BATCHER_RUNTIME(TensorFlowLite, TensorFlowLiteModel);
+}  // namespace
