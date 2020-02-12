4a75a7dc4d68f5f3621e162e42aaf14a0cb702ad
Zhao Lufan <zhao.lufan30@zte.com.cn>
2020-02-12 17:27:09 +0800

    Implement `TensorFlowLiteModel::create`
    Signed-off-by: Zhao Lufan <zhao.lufan30@zte.com.cn>

--- adlik_serving/runtime/tensorflow_lite/tensorflow_lite_runtime.cc
+++ adlik_serving/runtime/tensorflow_lite/tensorflow_lite_runtime.cc
@@ -7,19 +7,39 @@
 
 namespace {
 
+using adlik::serving::Batch;
+using adlik::serving::BatchingMessageTask;
 using adlik::serving::BatchingModel;
+using adlik::serving::BatchProcessor;
 using adlik::serving::CompositeBatchProcessor;
 using adlik::serving::ModelConfig;
 using adlik::serving::ModelId;
 
-class TensorFlowLiteModel : public CompositeBatchProcessor, public BatchingModel {
-public:
-  static cub::Status create(const ModelConfig& modelConfig,
-                            const ModelId& modelId,
-                            std::unique_ptr<TensorFlowLiteModel>* model) {
+class TensorFlowLiteBatchProcessor : public BatchProcessor {
+  virtual tensorflow::Status processBatch(Batch<BatchingMessageTask>& batch) override {
     throw std::logic_error("TODO");
   }
 };
 
+class TensorFlowLiteModel : public CompositeBatchProcessor, public BatchingModel {
+public:
+  IMPL_ROLE_NS(adlik::serving, BatchProcessor);
+
+  static cub::Status create(const ModelConfig& modelConfig,
+                            const ModelId& modelId,
+                            std::unique_ptr<TensorFlowLiteModel>* model) {
+    auto tempModel = std::make_unique<TensorFlowLiteModel>();
+
+    for (const auto& _ : modelConfig.instance_group()) {
+      tempModel->add(std::make_unique<TensorFlowLiteBatchProcessor>());
+    }
+
+    *model = std::move(tempModel);
+
+    return cub::Success;
+  }
+};
+
 DEFINE_UNIQUE_BATCHER_RUNTIME(TensorFlowLite, TensorFlowLiteModel);
+
 }  // namespace
