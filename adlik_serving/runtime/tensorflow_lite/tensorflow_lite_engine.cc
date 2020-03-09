077dbc35f2b53f9ddaf15c37a929259c59455faf
Zhao Lufan <zhao.lufan30@zte.com.cn>
2020-03-09 09:33:11 +0800

    Set inputs
    Signed-off-by: Zhao Lufan <zhao.lufan30@zte.com.cn>

--- adlik_serving/runtime/tensorflow_lite/tensorflow_lite_engine.cc
+++ adlik_serving/runtime/tensorflow_lite/tensorflow_lite_engine.cc
@@ -3,6 +3,8 @@
 
 #include "adlik_serving/runtime/tensorflow_lite/tensorflow_lite_engine.h"
 
+#include "adlik_serving/runtime/provider/predict_request_provider.h"
+
 using absl::Hash;
 using absl::string_view;
 using std::unordered_map;
@@ -11,9 +13,23 @@ using tflite::Interpreter;
 
 namespace adlik {
 namespace serving {
+namespace {
+void setInputs(Interpreter& interpreter,
+               const unordered_map<string_view, int, Hash<string_view>>& inputIndexMap,
+               Batch<BatchingMessageTask>& batch) {
+  auto numTasks = batch.num_tasks();
+
+  for (auto i = 0; i != numTasks; ++i) {
+    const auto& task = batch.task(i);
+  }
+}
+}  // namespace
+
 Status processTensorFlowLiteTask(Interpreter& interpreter,
                                  const unordered_map<string_view, int, Hash<string_view>>& inputIndexMap,
                                  Batch<BatchingMessageTask>& batch) {
+  setInputs(interpreter, inputIndexMap, batch);
+
   throw std::logic_error("Not implemented");
 }
 }  // namespace serving
