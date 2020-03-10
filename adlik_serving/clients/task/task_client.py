4fb816fadda2f30cabe0d72a0c4bc54c6721c6ed
jinshiying <jin.shiying@zte.com.cn>
2020-03-10 15:50:27 +0800

    Support amc case
    Signed-off-by: jinshiying <jin.shiying@zte.com.cn>

--- adlik_serving/clients/task/task_client.py
+++ adlik_serving/clients/task/task_client.py
@@ -2,10 +2,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """
-This is a sample of create and run k-means task use grpc protocol
+This is a sample of create use grpc protocol, just for test grid and amc case
 """
 
 import argparse
+import random
 import time
 
 from adlik_serving.apis import task_pb2, task_service_pb2_grpc
@@ -14,12 +15,16 @@ import grpc
 FLAGS = None
 
 
-def _create_request():
+def _create_header():
     request = task_pb2.CreateTaskRequest()
     request.model_spec.name = FLAGS.model_name
     request.task_type = 1  # Training task
-    # request.algorithm = 'k-means'
     request.is_sync = FLAGS.is_sync
+    return request
+
+
+def _create_grid_request():
+    request = _create_header()
     request.task.grid.n_clusters = FLAGS.clusters
     request.task.grid.input = FLAGS.input
     request.task.grid.max_iter = FLAGS.max_iter
@@ -29,10 +34,19 @@ def _create_request():
     return request
 
 
+def _create_amc_request():
+    request = _create_header()
+    max_bler_num = 180
+    for i in range(random.randint(1, max_bler_num)):
+        request.task.amc.blers[i] = random.random()
+    return request
+
+
 def _main():
     channel = grpc.insecure_channel(FLAGS.url)
     stub = task_service_pb2_grpc.TaskServiceStub(channel)
-    task_request = _create_request()
+    create_funcs = {'grid': _create_grid_request, 'amc': _create_amc_request}
+    task_request = create_funcs[FLAGS.model_name]()
     print('Create task request is: \n{}\n'.format(task_request))
     start = time.time()
     response = stub.create(task_request)
@@ -45,7 +59,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose', action="store_true", required=False, default=True,
                         help='Enable verbose output')
-    parser.add_argument('-m', '--model-name', type=str, required=False, default="grid",
+    parser.add_argument('-m', '--model-name', type=str, required=False, default="grid", choices=['grid', "amc"],
                         help='Name of model')
     parser.add_argument('-u', '--url', type=str, required=False, default='localhost:8500',
                         help='Adlik serving server URL. Default is localhost:8500.')
@@ -58,9 +72,9 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--is-sync', type=bool, required=False, default=True,
                         help='Whether run task synchronously, wait result until task is done if synchronous. '
                              'Default is True.')
-    parser.add_argument('-i', '--input', type=str, required=False, default='/home/jsy/ai/code/Adlik/adlik_serving/runtime/ml/algorithm/grid/test_data/good_data2.csv',
+    parser.add_argument('-i', '--input', type=str, required=True,
                         help='File path of input csv.')
-    parser.add_argument('-o', '--output', type=str, required=False, default='/home/jsy/ai/code/Adlik/adlik_serving/runtime/ml/algorithm/grid/test_data/result.csv',
+    parser.add_argument('-o', '--output', type=str, required=True,
                         help='File path of output csv.')
 
     FLAGS = parser.parse_args()
