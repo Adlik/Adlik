d11efeab1d84608ac990cf5b52d0702f73cb453e
Zhao Lufan <zhao.lufan30@zte.com.cn>
2020-05-15 16:27:59 +0800

    Add copyright for test codes
    Signed-off-by: Zhao Lufan <zhao.lufan30@zte.com.cn>

--- model_compiler_2/tests/model_compiler/compilers/test_keras_model_to_tf_model.py
+++ model_compiler_2/tests/model_compiler/compilers/test_keras_model_to_tf_model.py
@@ -1,3 +1,6 @@
+# Copyright 2019 ZTE corporation. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from unittest import TestCase
 
 import keras
--- model_compiler_2/tests/model_compiler/test_init.py
+++ model_compiler_2/tests/model_compiler/test_init.py
@@ -1,3 +1,6 @@
+# Copyright 2019 ZTE corporation. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
 from os import path
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 from unittest import TestCase
