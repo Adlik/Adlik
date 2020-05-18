02dc6fc0ea4886ea9130d443829d4ae1b014e290
Zhao Lufan <zhao.lufan30@zte.com.cn>
2020-05-18 15:34:25 +0800

    Build Protocol Buffers file before running tests
    Signed-off-by: Zhao Lufan <zhao.lufan30@zte.com.cn>

--- model_compiler_2/setup.py
+++ model_compiler_2/setup.py
@@ -4,45 +4,50 @@
 import os
 import shutil
 import subprocess
-from distutils.command.build_py import build_py
 from os import path
 from tempfile import TemporaryDirectory
 from urllib import request
 
 import setuptools
+from setuptools.command.build_py import build_py
 
 _TYPES_PROTO_URL = 'https://github.com/tensorflow/tensorflow/raw/v2.2.0/tensorflow/core/framework/types.proto'
+_PROJECT_PATH = path.dirname(path.abspath(__file__))
 
 
-class _MyBuildPy(build_py):
+def _generate_protos():
+    with TemporaryDirectory() as external_dir:
+        types_proto_dir = path.join(external_dir, 'tensorflow', 'core', 'framework')
+        types_proto_file = path.join(types_proto_dir, 'types.proto')
+        protos_dir = path.join(_PROJECT_PATH, 'src', 'model_compiler', 'protos')
+        protos_src_dir = path.join(protos_dir, 'src')
+        protos_generated_dir = path.join(protos_dir, 'generated')
+
+        os.makedirs(types_proto_dir, exist_ok=True)
+        os.makedirs(protos_generated_dir, exist_ok=True)
+
+        with request.urlopen(_TYPES_PROTO_URL) as response, open(types_proto_file, 'wb') as proto_file:
+            shutil.copyfileobj(response, proto_file)
+
+        subprocess.run(args=['protoc',
+                             '-I', protos_src_dir,
+                             '-I', external_dir,
+                             '--python_out', protos_generated_dir,
+                             path.join(protos_src_dir, 'model_config.proto')],
+                       check=True)
+
+
+class _BuildPy(build_py):
     def run(self):
-        root_path = path.dirname(path.abspath(__file__))
-
-        with TemporaryDirectory() as external_dir:
-            types_proto_dir = path.join(external_dir, 'tensorflow/core/framework')
-
-            os.makedirs(types_proto_dir, exist_ok=True)
-
-            with request.urlopen(_TYPES_PROTO_URL) as response, \
-                    open(path.join(types_proto_dir, 'types.proto'), 'wb') as proto_file:
-                shutil.copyfileobj(response, proto_file)
-
-            os.makedirs(path.join(root_path, 'src/model_compiler/protos/generated'), exist_ok=True)
-
-            subprocess.run(args=['protoc',
-                                 '-I', path.join(root_path, 'src/model_compiler/protos/src'),
-                                 '-I', external_dir,
-                                 '--python_out', path.join(root_path, 'src/model_compiler/protos/generated'),
-                                 path.join(root_path, 'src/model_compiler/protos/src/model_config.proto')],
-                           check=True)
+        _generate_protos()
 
         super().run()
 
 
 setuptools.setup(
     name='model_compiler',
-    packages=setuptools.find_namespace_packages('src', exclude=['generated']),
-    cmdclass={'build_py': _MyBuildPy},
+    packages=setuptools.find_namespace_packages('src'),
+    cmdclass={'build_py': _BuildPy},
     package_dir={'': 'src'},
     package_data={'model_compiler': ['protos/src/*.proto']},
     install_requires=[
@@ -50,9 +55,7 @@ setuptools.setup(
         'tensorflow',
         'tf2onnx@git+https://github.com/onnx/tensorflow-onnx.git@3383ff9bfa50e49bf69863983c965e49b94c073b'
     ],
-    entry_points={
-        'console_scripts': ['model-compiler=model_compiler.main:main'],
-    },
+    entry_points={'console_scripts': ['model-compiler=model_compiler.main:main'], },
     extras_require={
         'test': [
             'bandit',
