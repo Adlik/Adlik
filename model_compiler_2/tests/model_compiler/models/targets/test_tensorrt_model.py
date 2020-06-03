26600b0bc4dede8782df8fdc1fb283f64ffc8142
EFanZh <efanzh@gmail.com>
2020-06-03 07:39:27 +0000

    Support both implicit and explicit batch size TensorRT model (#175)
    Signed-off-by: Zhao Lufan <zhao.lufan30@zte.com.cn>
    
    Co-authored-by: Zhao Lufan <zhao.lufan30@zte.com.cn>

--- model_compiler_2/tests/model_compiler/models/targets/test_tensorrt_model.py
+++ model_compiler_2/tests/model_compiler/models/targets/test_tensorrt_model.py
@@ -5,14 +5,16 @@ import os
 from tempfile import TemporaryDirectory
 from unittest import TestCase
 
+import numpy
 import pytest
 import tensorflow as tf
 import tensorflow.python.eager.context as eager_context
 import tensorrt
+from tensorrt import Builder, DataType, ElementWiseOperation, Logger, Weights
 
 import model_compiler.compilers.repository as compiler_repository
 from model_compiler.models.irs.tf_model import Input as TfInput, TensorFlowModel
-from model_compiler.models.targets.tensorrt_model import TensorRTModel
+from model_compiler.models.targets.tensorrt_model import Input, TensorRTModel
 from model_compiler.protos.generated.model_config_pb2 import ModelInput, ModelOutput
 
 
@@ -33,6 +35,31 @@ def _make_tensorrt_model() -> TensorRTModel:
                     config=config_type.from_json({'max_batch_size': 4}))
 
 
+def _make_implicit_batch_size_tensorrt_model() -> TensorRTModel:
+    with Logger() as logger, Builder(logger) as builder, builder.create_network() as network:
+        input_x = network.add_input(name='x', dtype=DataType.FLOAT, shape=[4])
+        input_y = network.add_input(name='y', dtype=DataType.FLOAT, shape=[4])
+
+        weight = network.add_constant(
+            shape=[4],
+            weights=Weights(a=numpy.array([2.0, 3.0, 4.0, 5.0], dtype=numpy.float32))
+        ).get_output(0)
+
+        output_z = network.add_elementwise(input1=network.add_elementwise(input1=input_x,
+                                                                          input2=input_y,
+                                                                          op=ElementWiseOperation.SUM).get_output(0),
+                                           input2=weight,
+                                           op=ElementWiseOperation.SUM).get_output(0)
+
+        output_z.name = 'z'
+
+        network.mark_output(tensor=output_z)
+
+        return TensorRTModel(inputs=[Input(name='x'), Input(name='y')],
+                             outputs='z',
+                             cuda_engine=builder.build_cuda_engine(network))
+
+
 @pytest.mark.gpu_test
 class KerasModelFileTestCase(TestCase):
     def test_get_inputs(self):
@@ -47,6 +74,19 @@ class KerasModelFileTestCase(TestCase):
 
         self.assertEqual(saved_model.get_outputs(),
                          [ModelOutput(name='z:0', data_type=tf.float32.as_datatype_enum, dims=[4])])
+
+    def test_get_inputs_implicit_batch_size(self):
+        saved_model = _make_implicit_batch_size_tensorrt_model()
+
+        self.assertEqual(saved_model.get_inputs(),
+                         [ModelInput(name='x', data_type=tf.float32.as_datatype_enum, format=None, dims=[4]),
+                          ModelInput(name='y', data_type=tf.float32.as_datatype_enum, format=None, dims=[4])])
+
+    def test_get_outputs_implicit_batch_size(self):
+        saved_model = _make_implicit_batch_size_tensorrt_model()
+
+        self.assertEqual(saved_model.get_outputs(),
+                         [ModelOutput(name='z', data_type=tf.float32.as_datatype_enum, dims=[4])])
 
     def test_save(self):
         saved_model = _make_tensorrt_model()
