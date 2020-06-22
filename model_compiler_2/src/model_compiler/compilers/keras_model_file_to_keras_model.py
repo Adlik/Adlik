f7dd85ee251fd98e6a0f2f2dfeda415d9f18e4ed
EFanZh <efanzh@gmail.com>
2020-06-22 09:19:34 +0000

    Fix Keras coverage (#199)
    Signed-off-by: Zhao Lufan <zhao.lufan30@zte.com.cn>
    
    Co-authored-by: Zhao Lufan <zhao.lufan30@zte.com.cn>

--- model_compiler_2/src/model_compiler/compilers/keras_model_file_to_keras_model.py
+++ model_compiler_2/src/model_compiler/compilers/keras_model_file_to_keras_model.py
@@ -3,7 +3,6 @@
 
 import importlib.util
 
-import keras
 import tensorflow as tf
 
 from . import repository
@@ -22,19 +21,16 @@ def _load_module(file_path, name):
 
 def _get_custom_objects(file_path):
     script_module_name = ''
-    keras_custom_objects = {}
-    tf_custom_objects = {}
+    custom_objects = {}
 
     module = _load_module(file_path, script_module_name)
 
     for name, value in vars(module).items():
         if isinstance(value, type) and value.__module__ == script_module_name:
-            if issubclass(value, keras.layers.Layer):
-                keras_custom_objects[name] = value
-            elif issubclass(value, tf.keras.layers.Layer):
-                tf_custom_objects[name] = value
+            if issubclass(value, tf.keras.layers.Layer):
+                custom_objects[name] = value
 
-    return keras_custom_objects, tf_custom_objects
+    return custom_objects
 
 
 @repository.REPOSITORY.register(source_type=KerasModelFile, target_type=KerasModel)
@@ -42,22 +38,12 @@ def compile_source(source: KerasModelFile) -> KerasModel:
     with tf.Graph().as_default():
         if source.script_path:
             with tf.compat.v1.Session(graph=tf.Graph()):
-                keras_custom_objects, tf_custom_objects = _get_custom_objects(source.script_path)
+                custom_objects = _get_custom_objects(source.script_path)
 
             with tf.compat.v1.Session().as_default() as session:
-                try:
-                    model = keras.models.load_model(source.model_path,
-                                                    custom_objects=keras_custom_objects,
-                                                    compile=False)
-                except ValueError:
-                    model = tf.keras.models.load_model(source.model_path,
-                                                       custom_objects=tf_custom_objects,
-                                                       compile=False)
+                model = tf.keras.models.load_model(source.model_path, custom_objects=custom_objects, compile=False)
         else:
             with tf.compat.v1.Session().as_default() as session:
-                try:
-                    model = keras.models.load_model(source.model_path, custom_objects=None, compile=False)
-                except ValueError:
-                    model = tf.keras.models.load_model(source.model_path, custom_objects=None, compile=False)
+                model = tf.keras.models.load_model(source.model_path, custom_objects=None, compile=False)
 
     return KerasModel(model=model, session=session)
