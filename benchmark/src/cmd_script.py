49d6842d8d90e85cb2aebaf449cc0c8879244fc6
zhangkaili <zhang.kaili@zte.com.cn>
2020-07-13 19:39:13 +0800

    Fix the bug of benchmark test big model
    Closes #217
    
    Signed-off-by: zhangkaili <zhang.kaili@zte.com.cn>

--- benchmark/src/cmd_script.py
+++ benchmark/src/cmd_script.py
@@ -7,6 +7,18 @@ The CMD script
 
 import subprocess
 import argparse
+import os
+import time
+from grpc._channel import _InactiveRpcError
+from adlik_serving import PredictContext  # pylint: disable=import-error
+
+
+def _test_connect_of_serving(args):
+    try:
+        PredictContext(os.getenv('MODEL_NAME'), url=args.url, protocol=args.protocol, verbose=True)
+        return True
+    except _InactiveRpcError:
+        return False
 
 
 def _main(args):
@@ -15,17 +27,20 @@ def _main(args):
     client_command = ['sh', '-c', args.client_script]
     subprocess.run(compile_command)
     with subprocess.Popen(serving_command) as process:
+        while not _test_connect_of_serving(args):
+            time.sleep(1)
         subprocess.run(client_command)
         process.kill()
 
 
 if __name__ == '__main__':
     ARGS_PARSER = argparse.ArgumentParser()
-    ARGS_PARSER.add_argument('-s', '--serving-script', type=str, required=True,
-                             help='The serving script')
-    ARGS_PARSER.add_argument('-c', '--client-script', type=str, required=True,
-                             help='The client script')
-    ARGS_PARSER.add_argument('-cs', '--compile-script', type=str, required=True,
-                             help='The compile script')
+    ARGS_PARSER.add_argument('-s', '--serving-script', type=str, required=True, help='The serving script')
+    ARGS_PARSER.add_argument('-c', '--client-script', type=str, required=True, help='The client script')
+    ARGS_PARSER.add_argument('-cs', '--compile-script', type=str, required=True, help='The compile script')
+    ARGS_PARSER.add_argument('-u', '--url', type=str, required=False, default='localhost:8500',
+                             help='Server URL. Default is localhost:8500.')
+    ARGS_PARSER.add_argument('-i', '--protocol', type=str, required=False, default='grpc',
+                             help='Protocol ("http"/"grpc") used to ' + 'communicate with service. Default is "grpc".')
     PARSE_ARGS = ARGS_PARSER.parse_args()
     _main(PARSE_ARGS)
