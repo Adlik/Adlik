35fd7141b64224301048e4657442a505979ae00d
EFanZh <efanzh@gmail.com>
2020-07-14 12:33:32 +0000

    Format codes (#223)
    Signed-off-by: Zhao Lufan <zhao.lufan30@zte.com.cn>
    
    Co-authored-by: Zhao Lufan <zhao.lufan30@zte.com.cn>

--- model_compiler_2/src/model_compiler/compilers/__init__.py
+++ model_compiler_2/src/model_compiler/compilers/__init__.py
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from . import keras_model_file_to_keras_model, keras_model_to_tf_model, onnx_model_to_tensorrt_model, \
-    tf_frozen_graph_model_to_onnx_model, tf_model_to_saved_model, tf_model_to_tf_frozen_graph_model, \
-    tf_model_file_to_tf_model
+    tf_frozen_graph_model_to_onnx_model, tf_model_file_to_tf_model, tf_model_to_saved_model, \
+    tf_model_to_tf_frozen_graph_model
 
 __all__ = [
     'keras_model_file_to_keras_model',
