4d185fb039dd85387aea5aac2eba4930db8d949f
EFanZh <efanzh@gmail.com>
2020-07-20 01:59:55 +0000

    Load TensorRT modules lazily (#228)
    Signed-off-by: Zhao Lufan <zhao.lufan30@zte.com.cn>
    
    Co-authored-by: Zhao Lufan <zhao.lufan30@zte.com.cn>

--- model_compiler_2/src/model_compiler/models/targets/__init__.py
+++ model_compiler_2/src/model_compiler/models/targets/__init__.py
@@ -1,8 +1,16 @@
 # Copyright 2019 ZTE corporation. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+from types import ModuleType
+
 from . import saved_model
 
+try:
+    from . import tensorrt_model
+except ImportError:  # pragma: no cover
+    tensorrt_model = ModuleType('model_compiler.models.targets.tensorrt_model')
+
 __all__ = [
-    'saved_model'
+    'saved_model',
+    'tensorrt_model'
 ]
