88cce58e6e39f99ffdd097d8ad2e9c290c650c5a
kellyZhang <zhang.kaili@zte.com.cn>
2020-12-01 09:44:07 +0000

    Modify the tensorrt shell of script (#369)
    Signed-off-by: zhangkaili <zhang.kaili@zte.com.cn>

--- script/tensorrt.sh
+++ script/tensorrt.sh
@@ -11,6 +11,18 @@ elif [[ ${CUDA_VERSION} = '11.0' ]]; then
   apt-get install --no-install-recommends -y cuda-nvrtc-11-0
 fi
 
+if [[ ${TENSORRT_VERSION} = 7.0.* ]] ; then
+  apt-get install --no-install-recommends -y \
+    libcudnn7=*+cuda${CUDA_VERSION} \
+    libcudnn7-dev=*+cuda${CUDA_VERSION}
+elif [[ ${TENSORRT_VERSION} = 7.1.* ]] || [[ ${TENSORRT_VERSION} = 7.2.* ]] ; then
+  apt-get install --no-install-recommends -y \
+    libcudnn8=*+cuda${CUDA_VERSION} \
+    libcudnn8-dev=*+cuda${CUDA_VERSION}
+else
+  echo "Not support this CUDA version and TensorRT version"
+fi
+
 apt-get update && \
 apt-get install --no-install-recommends -y \
     libnvinfer7=${TENSORRT_VERSION}-1+cuda${CUDA_VERSION} \
@@ -22,19 +34,7 @@ apt-get install --no-install-recommends -y \
     python3-libnvinfer=${TENSORRT_VERSION}-1+cuda${CUDA_VERSION}
 
 if [[ ${TENSORRT_VERSION} = 7.0.* ]] ; then
-  apt-get install --no-install-recommends -y \
-    libcudnn7=*+cuda${CUDA_VERSION} \
-    libcudnn7-dev=*+cuda${CUDA_VERSION}
   apt-mark hold libcudnn7 libcudnn7-dev libnvinfer7 libnvinfer-dev libnvonnxparsers7 libnvonnxparsers-dev
 elif [[ ${TENSORRT_VERSION} = 7.1.* ]] || [[ ${TENSORRT_VERSION} = 7.2.* ]] ; then
-  if [[ ${CUDA_VERSION} = '10.2' ]]; then
-    apt-get autoremove --purge -y libcudnn8 \
-      libcudnn8-dev
-  fi
-  apt-get install --no-install-recommends -y \
-    libcudnn8=*+cuda${CUDA_VERSION} \
-    libcudnn8-dev=*+cuda${CUDA_VERSION}
   apt-mark hold libcudnn8 libcudnn8-dev libnvinfer7 libnvinfer-dev libnvonnxparsers7 libnvonnxparsers-dev
-else
-  echo "Not support this CUDA version and TensorRT version"
 fi
