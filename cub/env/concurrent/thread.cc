1d289eabce1777b4a4107166d5df3a3714ba2099
shiyingjin <jin.shiying@zte.com.cn>
2020-12-09 01:48:05 +0000

    Refactor thread class (#373)
    Signed-off-by: jinshiying <jin.shiying@zte.com.cn>

--- cub/env/concurrent/thread.cc
+++
@@ -1,9 +0,0 @@
-// Copyright 2019 ZTE corporation. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-#include "cub/env/concurrent/thread.h"
-
-namespace cub {
-
-Thread::~Thread() = default;
-}
