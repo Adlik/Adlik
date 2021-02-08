d9eeec0fcb6bda9aa97314953fce38c09288e15d
zhangkaili <zhang.kaili@zte.com.cn>
2021-02-08 15:28:59 +0800

    Model compiler add h5 to tflite path
    Closes #407
    
    Signed-off-by: zhangkaili <zhang.kaili@zte.com.cn>

---
+++ model_compiler/src/model_compiler/compilers/keras_model_file_to_tflite_model.py
@@ -0,0 +1,24 @@
+# Copyright 2019 ZTE corporation. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import tensorflow as tf
+
+from . import repository
+from ..models.sources.keras_model_file import KerasModelFile
+
+from ..models.targets.tflite_model import TfLiteModel
+from .. import tflite_util
+from .. import keras_util
+
+
+@repository.REPOSITORY.register(source_type=KerasModelFile, target_type=TfLiteModel, config_type=tflite_util.Config)
+def compile_source(source: KerasModelFile, config: tflite_util.Config) -> TfLiteModel:
+    if source.script_path:
+        custom_objects = keras_util.get_custom_objects(source.script_path)
+    else:
+        custom_objects = None
+
+    model = tf.keras.models.load_model(filepath=source.model_path, custom_objects=custom_objects, compile=False)
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    tflite_model = tflite_util.get_tflite_model(converter, config)
+    return TfLiteModel(tflite_model, config.input_formats)
--- model_compiler/src/model_compiler/compilers/saved_model_to_tflite_model.py
+++ model_compiler/src/model_compiler/compilers/saved_model_to_tflite_model.py
@@ -2,68 +2,13 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from tempfile import TemporaryDirectory
-from typing import Any, Iterable, List, Mapping, NamedTuple, Optional
 
 import tensorflow as tf
 
 from . import repository
-from .. import utilities
+from ..tflite_util import Config, get_tflite_model
 from ..models.targets.saved_model import SavedModel
 from ..models.targets.tflite_model import TfLiteModel
-
-
-def _parse_data_type(value: str) -> tf.DType:
-    try:
-        data_type = getattr(tf.dtypes, value)
-    except AttributeError as exception:
-        raise ValueError from exception
-
-    if isinstance(data_type, tf.DType):
-        return data_type
-
-    raise ValueError
-
-
-def _parse_op_set(value: str) -> tf.lite.OpsSet:
-    return tf.lite.OpsSet[value]
-
-
-class Config(NamedTuple):
-    optimization: bool = False
-    representative_dataset: Optional[tf.lite.RepresentativeDataset] = None
-    supported_ops: Optional[Iterable[tf.lite.OpsSet]] = None
-    supported_types: Optional[Iterable[tf.DType]] = None
-    inference_input_type: Optional[tf.DType] = tf.float32
-    inference_output_type: Optional[tf.DType] = tf.float32
-
-    @staticmethod
-    def from_json(value: Mapping[str, Any]) -> 'Config':
-        supported_types = value.get('supported_types')  # type: Optional[List[str]]
-        supported_ops = value.get('supported_ops')  # type: Optional[List[str]]
-
-        return Config(
-            optimization=value.get('optimization', False),
-            supported_ops=utilities.map_optional(supported_ops, lambda items: list(map(_parse_op_set, items))),
-            supported_types=utilities.map_optional(supported_types, lambda items: list(map(_parse_data_type, items))),
-            inference_input_type=utilities.map_optional(value.get('inference_input_type', 'float32'),
-                                                        _parse_data_type),
-            inference_output_type=utilities.map_optional(value.get('inference_output_type', 'float32'),
-                                                         _parse_data_type)
-        )
-
-    @staticmethod
-    def from_env(env: Mapping[str, str]) -> 'Config':
-        return Config(
-            optimization=bool(int(env.get('OPTIMIZATION', '0'))),
-            supported_ops=utilities.map_optional(utilities.split_by(env.get('SUPPORTED_OPS'), ','),
-                                                 lambda items: list(map(_parse_op_set, items))),
-            supported_types=utilities.map_optional(utilities.split_by(env.get('SUPPORTED_TYPES'), ','),
-                                                   lambda items: list(map(_parse_data_type, items))),
-            inference_input_type=utilities.map_optional(env.get('INFERENCE_INPUT_TYPE', 'float32'),
-                                                        _parse_data_type),
-            inference_output_type=utilities.map_optional(env.get('INFERENCE_OUTPUT_TYPE', 'float32'),
-                                                         _parse_data_type)
-        )
 
 
 @repository.REPOSITORY.register(source_type=SavedModel, target_type=TfLiteModel, config_type=Config)
@@ -72,23 +17,7 @@ def compile_source(source: SavedModel, config: Config) -> TfLiteModel:
         source.save(directory)
 
         converter = tf.lite.TFLiteConverter.from_saved_model(directory)
-
-        if config.optimization:
-            converter.optimizations.append(tf.lite.Optimize.DEFAULT)
-
-        converter.representative_dataset = config.representative_dataset
-
-        if config.supported_ops:
-            converter.target_spec.supported_ops.clear()
-            converter.target_spec.supported_ops.update(config.supported_ops)
-
-        if config.supported_types:
-            converter.target_spec.supported_types.extend(config.supported_types)
-
-        converter.inference_input_type = config.inference_input_type
-        converter.inference_output_type = config.inference_output_type
-
-        tflite_model = converter.convert()
+        tflite_model = get_tflite_model(converter, config)
         input_formats = [model_input.data_format for model_input in source.inputs]
 
         return TfLiteModel(tflite_model, input_formats)
