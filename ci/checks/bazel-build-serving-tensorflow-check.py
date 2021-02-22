5d821c21ad700ac93b7b5e26bb4dfe3337ea7754
NaNAGISaSA <cwangzju@126.com>
2021-02-22 17:33:07 +0800

    Opt CI Process
    Signed-off-by: NaNAGISaSA <cwangzju@126.com>

---
+++ ci/checks/bazel-build-serving-tensorflow-check.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Copyright 2019 ZTE corporation. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import subprocess
+import sys
+
+
+def _echo_var(value):
+    print(f'##vso[task.setvariable variable=BuildTensorFlow;isOutput=true]{value}')
+
+
+def main(args):
+    if args[0] == 'master':
+        _echo_var('true')
+        return
+
+    changed_files = subprocess.check_output(args=['git', 'diff', 'HEAD', 'origin/master', '--name-only'],
+                                            universal_newlines=True)
+    print("All changed files: ", changed_files)
+
+    need_to_build_tf = False
+    for file_path in changed_files.splitlines():
+        _, file_extension = os.path.splitext(file_path)
+        if file_extension in ('.h', '.cc'):
+            need_to_build_tf = True
+            break
+
+    _echo_var('true') if need_to_build_tf else _echo_var('false')
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
