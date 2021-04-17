8199654525e5a8a375da191a50e9cd3b3d5b27cb
zhangkaili <zhang.kaili@zte.com.cn>
2021-04-17 16:57:15 +0800

    Fix the bug of compile saved model file to saved model
    Signed-off-by: zhangkaili <zhang.kaili@zte.com.cn>

--- model_compiler/src/model_compiler/compilers/__init__.py
+++ model_compiler/src/model_compiler/compilers/__init__.py
@@ -5,7 +5,7 @@ from types import ModuleType
 
 from . import keras_model_file_to_keras_model, keras_model_to_tf_model, onnx_model_file_to_onnx_model, \
     onnx_model_to_openvino_model, saved_model_file_to_openvino_model, saved_model_file_to_tflite_model, \
-    saved_model_to_openvino_model, saved_model_to_tflite_model, \
+    saved_model_file_to_saved_model, saved_model_to_openvino_model, saved_model_to_tflite_model, \
     tf_frozen_graph_model_file_to_openvino_model, tf_frozen_graph_model_file_to_tf_model, \
     tf_frozen_graph_model_to_onnx_model, tf_model_file_to_tf_model, tf_model_to_saved_model, \
     tf_model_to_tf_frozen_graph_model
@@ -23,6 +23,7 @@ __all__ = [
     'onnx_model_to_tensorrt_model',
     'saved_model_file_to_openvino_model',
     'saved_model_file_to_tflite_model',
+    'saved_model_file_to_saved_model',
     'saved_model_to_openvino_model',
     'saved_model_to_tflite_model',
     'tf_frozen_graph_model_file_to_openvino_model',
--- model_compiler/src/model_compiler/compilers/saved_model_file_to_saved_model.py
+++ model_compiler/src/model_compiler/compilers/saved_model_file_to_saved_model.py
@@ -4,6 +4,7 @@
 import tensorflow as tf
 
 from . import repository
+from .. import utilities
 from ..models.sources.saved_model_file import SavedModelFile
 from ..models.targets.saved_model import SavedModel, Input, Output
 from ..tensorflow_util import Config, get_inputs
@@ -11,7 +12,8 @@ from ..tensorflow_util import Config, get_inputs
 
 @repository.REPOSITORY.register(source_type=SavedModelFile, target_type=SavedModel, config_type=Config)
 def compile_source(source: SavedModelFile, config: Config) -> SavedModel:
-    with tf.compat.v1.Session(graph=tf.Graph()) as sess:
+    with tf.Graph().as_default(), \
+         tf.compat.v1.Session(config=utilities.get_tf_cpu_only_config()).as_default() as sess:
         tf.compat.v1.saved_model.loader.load(sess,
                                              [tf.compat.v1.saved_model.tag_constants.SERVING],
                                              source.model_path)
