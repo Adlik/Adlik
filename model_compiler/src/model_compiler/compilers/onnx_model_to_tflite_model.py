9deb11112b98194ab7718f55d8c43755f6431f81
kellyZhang <zhang.kaili@zte.com.cn>
2021-04-21 06:03:01 +0000

    Compile onnx model to tf model (#248)
    Closes #246
    
    Signed-off-by: zhangkaili <zhang.kaili@zte.com.cn>

---
+++ model_compiler/src/model_compiler/compilers/onnx_model_to_tflite_model.py
@@ -0,0 +1,23 @@
+# Copyright 2019 ZTE corporation. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from tempfile import TemporaryDirectory
+import tensorflow as tf
+from onnx_tf.backend import prepare
+
+from . import repository
+from ..models.irs.onnx_model import OnnxModel
+from ..models.targets.tflite_model import TfLiteModel
+
+
+@repository.REPOSITORY.register(source_type=OnnxModel, target_type=TfLiteModel)
+def compile_source(source: OnnxModel) -> TfLiteModel:
+    tf_representation = prepare(source.model_proto)
+    with TemporaryDirectory() as directory:
+        tf_representation.export_graph(directory)
+        converter = tf.lite.TFLiteConverter.from_saved_model(directory)
+        converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS, tf.lite.OpsSet.SELECT_TF_OPS]
+
+        tflite_model = converter.convert()
+
+        return TfLiteModel(tflite_model, source.input_data_formats)
