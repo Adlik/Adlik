f209058942ca21cf3f1b873177028c6e40c612c8
NaNAGISaSA <cwangzju@126.com>
2021-05-31 14:51:46 +0800

    Reorder Imports
    Signed-off-by: NaNAGISaSA <cwangzju@126.com>

--- model_compiler/src/model_compiler/compilers/mxnet_model_file_to_onnx_model.py
+++ model_compiler/src/model_compiler/compilers/mxnet_model_file_to_onnx_model.py
@@ -1,9 +1,8 @@
 # Copyright 2019 ZTE corporation. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-import mxnet as mx
-import numpy as np
-import onnx
-
 from mxnet.contrib import onnx as onnx_mxnet
 
+import numpy as np
+import onnx
+import mxnet as mx
