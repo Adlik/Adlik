a2478affcf83e32f8c39c0ab420473a8f7981832
NaNAGISaSA <cwangzju@126.com>
2021-05-31 09:52:10 +0800

    Add Model File
    Signed-off-by: NaNAGISaSA <cwangzju@126.com>

--- model_compiler/src/model_compiler/compilers/__init__.py
+++ model_compiler/src/model_compiler/compilers/__init__.py
@@ -4,11 +4,12 @@
 from types import ModuleType
 
 from . import caffe_model_file_to_onnx_model, keras_model_file_to_keras_model, keras_model_file_to_tflite_model, \
-    keras_model_to_tf_model, onnx_model_file_to_onnx_model, onnx_model_to_openvino_model, onnx_model_to_tflite_model, \
-    saved_model_file_to_openvino_model, saved_model_file_to_saved_model, saved_model_file_to_tflite_model, \
-    saved_model_to_openvino_model, saved_model_to_tflite_model, tf_frozen_graph_model_file_to_openvino_model, \
-    tf_frozen_graph_model_file_to_tf_model, tf_frozen_graph_model_to_onnx_model, tf_model_file_to_tf_model, \
-    tf_model_to_saved_model, tf_model_to_tf_frozen_graph_model
+    keras_model_to_tf_model, mxnet_model_file_to_onnx_model, onnx_model_file_to_onnx_model, \
+    onnx_model_to_openvino_model, onnx_model_to_tflite_model, saved_model_file_to_openvino_model, \
+    saved_model_file_to_saved_model, saved_model_file_to_tflite_model, saved_model_to_openvino_model, \
+    saved_model_to_tflite_model, tf_frozen_graph_model_file_to_openvino_model, tf_frozen_graph_model_file_to_tf_model, \
+    tf_frozen_graph_model_to_onnx_model, tf_model_file_to_tf_model, tf_model_to_saved_model, \
+    tf_model_to_tf_frozen_graph_model
 
 try:
     from . import onnx_model_to_tensorrt_model
@@ -21,6 +22,7 @@ __all__ = [
     'keras_model_file_to_tflite_model',
     'keras_model_to_tf_model',
     'onnx_model_file_to_onnx_model',
+    'mxnet_model_file_to_onnx_model',
     'onnx_model_to_openvino_model',
     'onnx_model_to_tensorrt_model',
     'onnx_model_to_tflite_model',
