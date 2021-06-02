1f55e9d8fd253d1753055aba13b4c2c8ea0f4a1b
xueshe <943979232@qq.com>
2021-06-02 15:54:57 +0800

    Add torch
    Signed-off-by: xueshe <943979232@qq.com>

--- model_compiler/src/model_compiler/models/data_type.py
+++ model_compiler/src/model_compiler/models/data_type.py
@@ -6,6 +6,7 @@
 import enum
 from enum import Enum
 from onnx import TensorProto as OnnxTensorProto
+import torch
 from tensorflow.core.framework.types_pb2 import DataType as TfDataType  # pylint: disable=no-name-in-module
 
 _ONNX_DATA_TYPE = OnnxTensorProto.DataType  # pylint: disable=no-member
