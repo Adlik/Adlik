8eb0f025a2980ba66d5d6a50b0b84ff478dbfce8
zhangkaili <zhang.kaili@zte.com.cn>
2021-06-07 10:44:41 +0800

    Fix the bug of compile saved model file to saved model
    Closes #466
    
    Signed-off-by: zhangkaili <zhang.kaili@zte.com.cn>

--- model_compiler/src/model_compiler/compilers/saved_model_file_to_saved_model.py
+++ model_compiler/src/model_compiler/compilers/saved_model_file_to_saved_model.py
@@ -1,23 +1,84 @@
 # Copyright 2019 ZTE corporation. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+from typing import Any, Mapping, NamedTuple, Optional, Sequence
 import tensorflow as tf
 
 from . import repository
-from .. import utilities
 from ..models.sources.saved_model_file import SavedModelFile
-from ..models.targets.saved_model import SavedModel, Input, Output
-from ..tensorflow_util import Config, get_inputs
+from ..models.targets.saved_model import SavedModel, Input, Output, DataFormat
+from .. import utilities
+
+
+class Config(NamedTuple):
+    input_names: Optional[Sequence[str]] = None
+    data_formats: Sequence[Optional[DataFormat]] = []
+    output_names: Optional[Sequence[str]] = None
+    signature_keys: Optional[str] = None
+
+    @staticmethod
+    def from_json(value: Mapping[str, Any]) -> 'Config':
+        return Config(input_names=value.get('input_names'),
+                      data_formats=utilities.get_data_formats(value.get('input_formats')),
+                      output_names=value.get('output_names'),
+                      signature_keys=value.get('signature_keys'))
+
+    @staticmethod
+    def from_env(env: Mapping[str, str]) -> 'Config':
+        return Config(input_names=utilities.split_by(env.get('INPUT_NAMES'), ','),
+                      data_formats=utilities.get_data_formats(utilities.split_by('INPUT_FORMATS', ',')),
+                      output_names=utilities.split_by(env.get('OUTPUT_NAMES'), ','),
+                      signature_keys=env.get('SIGNATURE_KEYS'))
+
+
+def get_model_info(signature):
+    model_layers = {}
+    if len(signature) == 1:
+        model_layers = signature
+    else:
+        for input_signature in signature:
+            if input_signature:
+                model_layers.update(input_signature)
+
+    return model_layers
+
+
+def _get_inputs(model_inputs, input_names, data_formats):
+    inputs = []
+    if input_names:
+        for i, input_name in enumerate(input_names):
+            inputs.append(Input(input_name, model_inputs[input_name],
+                                data_formats[i] if i < len(data_formats) else None))
+
+    else:
+        for i, input_name in enumerate(model_inputs.keys()):
+            inputs.append(Input(input_name, model_inputs[input_name],
+                                data_formats[i] if i < len(data_formats) else None))
+
+    return inputs
+
+
+def _get_outputs(model_outputs, output_names):
+    outputs = []
+    if output_names:
+        for output_name in output_names:
+            outputs.append(Output(output_name, model_outputs[output_name]))
+    else:
+        for key in model_outputs.keys():
+            outputs.append(Output(key, model_outputs[key]))
+    return outputs
 
 
 @repository.REPOSITORY.register(source_type=SavedModelFile, target_type=SavedModel, config_type=Config)
 def compile_source(source: SavedModelFile, config: Config) -> SavedModel:
-    with tf.Graph().as_default(), \
-         tf.compat.v1.Session(config=utilities.get_tf_cpu_only_config()).as_default() as sess:
-        tf.compat.v1.saved_model.loader.load(sess,
-                                             [tf.compat.v1.saved_model.tag_constants.SERVING],
-                                             source.model_path)
-        graph = tf.compat.v1.get_default_graph()
-        inputs = [Input(tensor.name, tensor, data_format) for tensor, data_format in get_inputs(graph, config)]
-        outputs = [Output(tensor.name, tensor) for tensor in config.get_output_tensors_from_graph(graph)]
-        return SavedModel(inputs=inputs, outputs=outputs, session=sess)
+    model = tf.saved_model.load(source.model_path, tags=['serve'])
+    if config.signature_keys:
+        infer = model.signatures[config.signature_keys]
+    else:
+        infer = model.signatures['serving_default']
+
+    return SavedModel(inputs=_get_inputs(get_model_info(infer.structured_input_signature),
+                                         config.input_names, config.data_formats),
+                      outputs=_get_outputs(get_model_info(infer.structured_outputs), config.output_names),
+                      session=None,
+                      model=model)
--- model_compiler/tests/model_compiler/compilers/test_saved_model_file_to_saved_model.py
+++ model_compiler/tests/model_compiler/compilers/test_saved_model_file_to_saved_model.py
@@ -7,9 +7,31 @@ from tempfile import TemporaryDirectory
 import tensorflow as tf
 
 import model_compiler.compilers.saved_model_file_to_saved_model as compiler
-from model_compiler.tensorflow_util import Config
+from model_compiler.compilers.saved_model_file_to_saved_model import Config
 from model_compiler.models.sources.saved_model_file import SavedModelFile
 from model_compiler.models.data_format import DataFormat
+
+
+class ConfigTestCase(TestCase):
+    def test_from_json(self):
+        self.assertEqual(Config.from_json({'input_names': ['x'],
+                                           'input_formats': ['channels_first', 'channels_last'],
+                                           'output_names': ['z'],
+                                           'signature_keys': 'predict'}),
+                         Config(input_names=['x'],
+                                data_formats=[DataFormat.CHANNELS_FIRST, DataFormat.CHANNELS_LAST],
+                                output_names=['z'],
+                                signature_keys='predict'))
+
+    def test_from_env(self):
+        self.assertEqual(Config.from_env({'INPUT_NAMES': 'x,y',
+                                          'INPUT_FORMATS': 'channels_last',
+                                          'OUTPUT_NAMES': 'z',
+                                          'SIGNATURE_KEYS': 'predict'}),
+                         Config(input_names=['x', 'y'],
+                                data_formats=[DataFormat.CHANNELS_LAST],
+                                output_names=['z'],
+                                signature_keys='predict'))
 
 
 def _save_saved_model_file(model_dir):
@@ -28,7 +50,7 @@ def _save_saved_model_file(model_dir):
 
         builder.add_meta_graph_and_variables(session, tags=[tf.compat.v1.saved_model.tag_constants.SERVING],
                                              signature_def_map={
-                                                 'predict': tf.compat.v1.saved_model.build_signature_def(
+                                                 'serving_default': tf.compat.v1.saved_model.build_signature_def(
                                                      inputs={'x': input_x_tensor_info,
                                                              'y': input_y_tensor_info},
                                                      outputs={'z': output_tensor_info})})
@@ -40,10 +62,38 @@ class CompileSourceTestCase(TestCase):
         with TemporaryDirectory() as model_dir:
             _save_saved_model_file(model_dir)
             compiled = compiler.compile_source(source=SavedModelFile(model_path=model_dir),
-                                               config=Config.from_json({"input_formats":
-                                                                        ['channels_first', 'channels_last']}))
+                                               config=Config.from_json({"input_formats": ['channels_first']}))
 
-        self.assertEqual([model_input.tensor.name for model_input in compiled.inputs], ['x:0', 'y:0'])
+        self.assertEqual(len(compiled.inputs), 2)
         self.assertEqual([model_input.data_format for model_input in compiled.inputs],
-                         [DataFormat.CHANNELS_FIRST, DataFormat.CHANNELS_LAST])
-        self.assertEqual([model_output.name for model_output in compiled.outputs], ['z:0'])
+                         [DataFormat.CHANNELS_FIRST, None])
+        self.assertEqual([model_output.name for model_output in compiled.outputs], ['z'])
+
+    def test_compile_with_names(self):
+        with TemporaryDirectory() as model_dir:
+            _save_saved_model_file(model_dir)
+            compiled = compiler.compile_source(source=SavedModelFile(model_path=model_dir),
+                                               config=Config.from_json({
+                                                   "input_names": ['x', 'y'],
+                                                   "output_names": ['z'],
+                                                   "input_formats": ['channels_first']}))
+
+        self.assertEqual([model_input.tensor.name for model_input in compiled.inputs], ['x', 'y'])
+        self.assertEqual([model_input.data_format for model_input in compiled.inputs],
+                         [DataFormat.CHANNELS_FIRST, None])
+        self.assertEqual([model_output.name for model_output in compiled.outputs], ['z'])
+
+    def test_compile_with_signature_keys(self):
+        with TemporaryDirectory() as model_dir:
+            _save_saved_model_file(model_dir)
+            compiled = compiler.compile_source(source=SavedModelFile(model_path=model_dir),
+                                               config=Config.from_json({
+                                                   "input_names": ['x', 'y'],
+                                                   "output_names": ['z'],
+                                                   "input_formats": ['channels_first'],
+                                                   "signature_keys": 'serving_default'}))
+
+        self.assertEqual([model_input.tensor.name for model_input in compiled.inputs], ['x', 'y'])
+        self.assertEqual([model_input.data_format for model_input in compiled.inputs],
+                         [DataFormat.CHANNELS_FIRST, None])
+        self.assertEqual([model_output.name for model_output in compiled.outputs], ['z'])
