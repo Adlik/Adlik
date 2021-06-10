e07b83c0486df8b6775bd9bff44e18cb8acd64e2
kellyZhang <zhang.kaili@zte.com.cn>
2021-06-10 02:59:25 +0000

    Fix the bug of compile saved model file to saved model (#467)
    Closes #466
    
    Signed-off-by: zhangkaili <zhang.kaili@zte.com.cn>
    
    Co-authored-by: amadeus-zte <54922692+amadeus-zte@users.noreply.github.com>

--- model_compiler/tests/model_compiler/models/targets/test_saved_model.py
+++ model_compiler/tests/model_compiler/models/targets/test_saved_model.py
@@ -25,6 +25,24 @@ def _make_saved_model() -> SavedModel:
                       session=session)
 
 
+def _make_tf2_saved_model() -> SavedModel:
+    origin_model = tf.keras.Sequential()
+    origin_model.add(tf.keras.layers.Dense(units=8, name='l1', input_shape=(16,)))
+    origin_model.add(tf.keras.layers.Dense(units=4, name='l2'))
+    origin_model.add(tf.keras.layers.Dense(units=2, name='l3'))
+    with TemporaryDirectory() as directory:
+        tf.saved_model.save(origin_model, directory)
+        model = tf.saved_model.load(directory)
+        infer = model.signatures['serving_default']
+
+    return SavedModel(inputs=[Input(name='l1_input',
+                                    tensor=infer.structured_input_signature[1]['l1_input'])],
+                      outputs=[Output(name='l3',
+                                      tensor=infer.structured_outputs['l3'])],
+                      session=None,
+                      model=model)
+
+
 class KerasModelFileTestCase(TestCase):
     def test_get_inputs(self):
         saved_model = _make_saved_model()
@@ -52,3 +70,14 @@ class KerasModelFileTestCase(TestCase):
 
     def test_get_platform(self):
         self.assertEqual(SavedModel.get_platform(), ('tensorflow', tf.version.VERSION))
+
+    def test_tf2_saved_model(self):
+        saved_model = _make_tf2_saved_model()
+
+        with TemporaryDirectory() as save_path:
+            saved_model.save(save_path)
+
+            self.assertEqual(sorted(os.listdir(save_path)), ['assets', 'saved_model.pb', 'variables'])
+
+            self.assertEqual(sorted(os.listdir(os.path.join(save_path, 'variables'))),
+                             ['variables.data-00000-of-00001', 'variables.index'])
