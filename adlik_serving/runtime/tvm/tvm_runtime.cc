4cb3f034e2b776395b8a848e0eb1a019c86a93b6
jinshiying <jin.shiying@zte.com.cn>
2021-06-20 09:48:49 +0800

    Support tvm runtime
    Signed-off-by: jinshiying <jin.shiying@zte.com.cn>

---
+++ adlik_serving/runtime/tvm/tvm_runtime.cc
@@ -0,0 +1,11 @@
+// Copyright 2019 ZTE corporation. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#include "adlik_serving/runtime/tvm/tvm_model.h"
+#include "adlik_serving/runtime/util/unique_batcher_runtime_helper.h"
+
+namespace tvm_runtime {
+
+DEFINE_UNIQUE_BATCHER_RUNTIME(tvm, TvmModel);
+
+}
