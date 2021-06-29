4b7d60d33a8adf10157bdeac09b989327ae3bace
zhangkaili <zhang.kaili@zte.com.cn>
2021-06-29 16:43:52 +0800

    Compiler supports normal use when the user does not install tvm
    Closes #478
    
    Signed-off-by: zhangkaili <zhang.kaili@zte.com.cn>

--- model_compiler/src/model_compiler/compilers/__init__.py
+++ model_compiler/src/model_compiler/compilers/__init__.py
@@ -4,18 +4,19 @@
 from types import ModuleType
 
 from . import caffe_model_file_to_onnx_model, keras_model_file_to_keras_model, keras_model_file_to_tflite_model, \
-    keras_model_to_tf_model, mxnet_model_file_to_onnx_model, keras_model_file_to_tvm_model, \
-    onnx_model_file_to_onnx_model, onnx_model_to_onnx_model_file, onnx_model_to_openvino_model, \
-    onnx_model_to_tflite_model, paddle_model_file_to_onnx_model, saved_model_file_to_openvino_model, \
-    saved_model_file_to_saved_model, saved_model_file_to_tflite_model, saved_model_file_to_tftrt_saved_model, \
-    saved_model_to_openvino_model, saved_model_to_tflite_model, tf_frozen_graph_model_file_to_openvino_model, \
-    tf_frozen_graph_model_file_to_tf_model, tf_frozen_graph_model_to_onnx_model, tf_model_file_to_tf_model, \
-    tf_model_to_saved_model, tf_model_to_tf_frozen_graph_model, torch_model_file_to_onnx_model
+    keras_model_to_tf_model, mxnet_model_file_to_onnx_model, onnx_model_file_to_onnx_model,\
+    onnx_model_to_onnx_model_file, onnx_model_to_openvino_model, onnx_model_to_tflite_model, \
+    paddle_model_file_to_onnx_model, saved_model_file_to_openvino_model, saved_model_file_to_saved_model, \
+    saved_model_file_to_tflite_model, saved_model_file_to_tftrt_saved_model, saved_model_to_openvino_model, \
+    saved_model_to_tflite_model, tf_frozen_graph_model_file_to_openvino_model, tf_frozen_graph_model_file_to_tf_model, \
+    tf_frozen_graph_model_to_onnx_model, tf_model_file_to_tf_model, tf_model_to_saved_model, \
+    tf_model_to_tf_frozen_graph_model, torch_model_file_to_onnx_model
 
 try:
-    from . import onnx_model_to_tensorrt_model
+    from . import onnx_model_to_tensorrt_model, keras_model_file_to_tvm_model
 except ImportError:  # pragma: no cover
     onnx_model_to_tensorrt_model = ModuleType('model_compiler.compilers.onnx_model_to_tensorrt_model')
+    keras_model_file_to_tvm_model = ModuleType('model_compiler.compilers.keras_model_file_to_tvm_model')
 
 __all__ = [
     'caffe_model_file_to_onnx_model',
@@ -23,7 +24,6 @@ __all__ = [
     'keras_model_file_to_tflite_model',
     'keras_model_to_tf_model',
     'mxnet_model_file_to_onnx_model',  # Need to import mxnet first and then tvm
-    'keras_model_file_to_tvm_model',
     'onnx_model_file_to_onnx_model',
     'onnx_model_to_onnx_model_file',
     'onnx_model_to_openvino_model',
