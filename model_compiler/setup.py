69e77c5035f6e40aced0030c9347c2eb517a8b06
xueshe <liu.guangcheng@zte.com.cn>
2021-10-29 06:47:35 +0000

    Delete TVM and MXnet (#528)
    Signed-off-by: 刘广呈 10300819 <liu.guangcheng@zte.com.cn>

--- model_compiler/setup.py
+++ model_compiler/setup.py
@@ -64,7 +64,6 @@ setuptools.setup(
         'tensorflow_addons',
         'test-generator==0.1.1',
         'tf2onnx',
-        'mxnet',
         'tornado==6.1'
     ],
     entry_points={'console_scripts': ['model-compiler=model_compiler.main:main']},
