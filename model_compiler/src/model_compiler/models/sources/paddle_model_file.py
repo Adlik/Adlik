c157f5a153ecbe041a0b85ab7cc9c09f7d9e0679
靳伟昭 10307459 <jin.weizhao@zte.com.cn>
2022-01-29 11:03:37 +0800

    Fix source model type bug
    Signed-off-by: 靳伟昭 10307459 <jin.weizhao@zte.com.cn>

--- model_compiler/src/model_compiler/models/sources/paddle_model_file.py
+++ model_compiler/src/model_compiler/models/sources/paddle_model_file.py
@@ -1,6 +1,7 @@
 # Copyright 2019 ZTE corporation. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+from glob import glob
 from os import path
 from typing import Any, Mapping, NamedTuple
 
@@ -20,16 +21,24 @@ class PaddlePaddleModelFile(NamedTuple):
         return PaddlePaddleModelFile(model_path=env['PADDLE_PADDLE_PATH'])
 
     @staticmethod
+    def _is_paddle_file_dir(model_path: str):
+        return path.isdir(model_path) and len(glob(path.join(model_path, "*.pdiparams"))) and (
+               len(glob(path.join(model_path, "*.pdmodel"))) or
+               path.exists(path.join(model_path, "__model__")))
+
+    @staticmethod
     def accepts_kwargs(kwargs: Mapping[str, Any]) -> bool:
         input_model = kwargs.get('model_path')
 
-        return isinstance(input_model, str) and path.isdir(input_model)
+        return isinstance(input_model, str) and path.isdir(input_model) and \
+            PaddlePaddleModelFile._is_paddle_file_dir(input_model)
 
     @staticmethod
     def accepts_json(value: Mapping[str, Any]) -> bool:
         input_model = value.get('input_model')
 
-        return isinstance(input_model, str) and path.isdir(input_model)
+        return isinstance(input_model, str) and path.isdir(input_model) and \
+            PaddlePaddleModelFile._is_paddle_file_dir(input_model)
 
     @staticmethod
     def accepts_env(env: Mapping[str, str]) -> bool:
