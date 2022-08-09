09fe905778820bf1523ed49b8b4b4b6729f7ed95
wangjiayue <wang.jiayue@zte.com.cn>
2022-08-09 02:19:12 +0000

    Upgrade openvino of compiler to version 2022.1.0 (#592)
    Closes #584.
    
    Signed-off-by: 王佳越 10335419 <wang.jiayue@zte.com.cn>
    
    Co-authored-by: kellyZhang <zhang.kaili@zte.com.cn>

--- model_compiler/tests/model_compiler/compilers/test_tf_forzen_graph_file_to_openvino_model.py
+++ model_compiler/tests/model_compiler/compilers/test_tf_forzen_graph_file_to_openvino_model.py
@@ -17,13 +17,13 @@ class ConfigTestCase(TestCase):
         self.assertEqual(Config.from_json({'input_names': ['input'],
                                            'input_shapes': [[1, 2, 3, 4]],
                                            'output_names': ['output'],
-                                           'max_batch_size': 1,
-                                           'enable_nhwc_to_nchw': False}),
+                                           'max_batch_size': 1
+                                           }),
                          Config(input_names=['input'],
                                 input_shapes=[[1, 2, 3, 4]],
                                 output_names=['output'],
-                                max_batch_size=1,
-                                enable_nhwc_to_nchw=False))
+                                max_batch_size=1
+                                ))
 
     def test_from_json_no_names(self):
         self.assertEqual(Config.from_json({}),
@@ -33,13 +33,13 @@ class ConfigTestCase(TestCase):
         self.assertEqual(Config.from_env({'INPUT_NAMES': 'input',
                                           'INPUT_SHAPES': '[1, 2, 3, 4]',
                                           'OUTPUT_NAMES': 'output',
-                                          'MAX_BATCH_SIZE': '1',
-                                          'ENABLE_NHWC_TO_NCHW': '0'}),
+                                          'MAX_BATCH_SIZE': '1'
+                                          }),
                          Config(input_names=['input'],
                                 input_shapes=[[1, 2, 3, 4]],
                                 output_names=['output'],
-                                max_batch_size=1,
-                                enable_nhwc_to_nchw=False))
+                                max_batch_size=1
+                                ))
 
     def test_from_env_no_names(self):
         self.assertEqual(Config.from_env({}),
@@ -51,6 +51,21 @@ def _save_frozen_graph_model(model_file):
         input_x = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, 2, 3, 4], name='x')
         input_y = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, 2, 3, 4], name='y')
         weight = tf.Variable(initial_value=4.2, dtype=tf.float32)
+        tf.multiply(input_x + input_y, weight, name='z')
+
+        session.run(weight.initializer)
+
+        constant_graph = tf.compat.v1.graph_util.convert_variables_to_constants(session, session.graph_def, ['z'])
+
+    with open(model_file.name, mode='wb') as graph_file:
+        graph_file.write(constant_graph.SerializeToString())
+
+
+def _save_frozen_graph_model_fp16(model_file):
+    with tf.compat.v1.Session(graph=tf.Graph()) as session:
+        input_x = tf.compat.v1.placeholder(dtype=tf.float16, shape=[None, 2, 3, 4], name='x')
+        input_y = tf.compat.v1.placeholder(dtype=tf.float16, shape=[None, 2, 3, 4], name='y')
+        weight = tf.Variable(initial_value=4.2, dtype=tf.float16)
         tf.multiply(input_x + input_y, weight, name='z')
 
         session.run(weight.initializer)
@@ -77,7 +92,7 @@ class CompileSourceTestCase(TestCase):
 
     def test_compile_with_fp16(self):
         with NamedTemporaryFile(suffix='.pb') as model_file:
-            _save_frozen_graph_model(model_file)
+            _save_frozen_graph_model_fp16(model_file)
             config = Config.from_json({'max_batch_size': 1,
                                        'data_type': 'FP16'})
             compiled = compiler.compile_source(FrozenGraphFile(model_path=model_file.name), config)
@@ -94,7 +109,6 @@ class CompileSourceTestCase(TestCase):
             _save_frozen_graph_model(model_file)
             config = Config.from_json({'input_names': ['x', 'y'],
                                        'output_names': ['z'],
-                                       'enable_nhwc_to_nchw': False,
                                        'max_batch_size': 1})
             compiled = compiler.compile_source(FrozenGraphFile(model_path=model_file.name), config)
             self.assertEqual(compiled.get_inputs(),
@@ -110,8 +124,8 @@ class CompileSourceTestCase(TestCase):
             _save_frozen_graph_model(model_file)
             config = Config.from_json({'input_names': ['x', 'y'],
                                        'input_shapes': [[1, 2, 3, 4], [1, 2, 3, 4]],
-                                       'output_names': ['z'],
-                                       'enable_nhwc_to_nchw': False})
+                                       'output_names': ['z']
+                                       })
             compiled = compiler.compile_source(FrozenGraphFile(model_path=model_file.name), config)
             self.assertEqual(compiled.get_inputs(),
                              [ModelInput(name='x', data_type=tf.float32.as_datatype_enum,
@@ -120,19 +134,3 @@ class CompileSourceTestCase(TestCase):
                                          format=ModelInput.FORMAT_NONE, dims=[2, 3, 4])])  # pylint: disable=no-member
             self.assertEqual(compiled.get_outputs(),
                              [ModelOutput(name='z', data_type=tf.float32.as_datatype_enum, dims=[2, 3, 4])])
-
-    def test_compile_with_all_params_with_enable_nhwc_to_nchw_true(self):
-        with NamedTemporaryFile(suffix='.pb') as model_file:
-            _save_frozen_graph_model(model_file)
-            config = Config.from_json({'input_names': ['x', 'y'],
-                                       'output_names': ['z'],
-                                       'enable_nhwc_to_nchw': True,
-                                       'max_batch_size': 1})
-            compiled = compiler.compile_source(FrozenGraphFile(model_path=model_file.name), config)
-            self.assertEqual(compiled.get_inputs(),
-                             [ModelInput(name='x', data_type=tf.float32.as_datatype_enum,
-                                         format=ModelInput.FORMAT_NONE, dims=[4, 2, 3]),  # pylint: disable=no-member
-                              ModelInput(name='y', data_type=tf.float32.as_datatype_enum,
-                                         format=ModelInput.FORMAT_NONE, dims=[4, 2, 3])])  # pylint: disable=no-member
-            self.assertEqual(compiled.get_outputs(),
-                             [ModelOutput(name='z', data_type=tf.float32.as_datatype_enum, dims=[4, 2, 3])])
