b205d6511674514690f901cf599de164ed0d2df6
Shi mei <shi.mei1@zte.com.cn>
2023-02-08 17:41:42 +0800

    Add enflame parameters for compilation
    Signed-off-by: Shi mei <shi.mei1@zte.com.cn>

--- model_compiler/src/model_compiler/compilers/onnx_model_file_to_enflame_model.py
+++ model_compiler/src/model_compiler/compilers/onnx_model_file_to_enflame_model.py
@@ -1,10 +1,11 @@
 # Copyright 2019 ZTE corporation. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-from typing import Any, Mapping, NamedTuple, Optional, Sequence
+from typing import Any, Mapping, NamedTuple, Optional, List, Sequence
 
 import onnx
 import onnx.utils
+import json
 
 from . import repository
 from .. import utilities
@@ -16,16 +17,25 @@ from ..models.targets.enflame_model import EnflameModel
 
 class Config(NamedTuple):
     input_formats: Sequence[Optional[DataFormat]]
+    resource_mode: Optional[str]
+    build_flag: Optional[str]
+    input_shapes: List[List]
 
     @staticmethod
     def from_json(value: Mapping[str, Any]) -> 'Config':
         return Config(input_formats=utilities.get_data_formats(
-            value.get('input_formats')))
+            value.get('input_formats')),
+                      input_shapes=value.get('input_shapes'),
+                      resource_mode=value.get('resource_mode'),
+                      build_flag=value.get('build_flag'))
 
     @staticmethod
     def from_env(env: Mapping[str, str]) -> 'Config':
         return Config(input_formats=utilities.get_data_formats(
-            utilities.split_by(env.get('INPUT_FORMATS'), ',')))
+            utilities.split_by(env.get('INPUT_FORMATS'), ',')),
+                      input_shapes=env.get('INPUT_SHAPES'),
+                      resource_mode=env.get('RESOURCE_MODE'),
+                      build_flag=env.get('BUILD_FLAG'))
 
 
 @repository.REPOSITORY.register(source_type=ONNXModelFile,
@@ -38,11 +48,23 @@ def compile_source(source: ONNXModelFile, config=Config) -> EnflameModel:
     initializers = {initializer.name for initializer in graph.initializer}
     with TopsInference.device(0, 0):
         onnx_parser = TopsInference.create_parser(TopsInference.ONNX_MODEL)
+        if config.input_shapes:
+            onnx_parser.set_input_shapes(config.input_shapes)
         network = onnx_parser.read(source.model_path)
+        compile_options = {'resource_mode': config.resource_mode}
         optimizer = TopsInference.create_optimizer()
+        optimizer.set_compile_options(json.dumps(compile_options))
+        if config.build_flag == 'TTopsInference.KFP16_MIX':
+            optimizer.set_build_flag(TopsInference.KFP16_MIX)
+        if config.build_flag == 'TopsInference.KFP16':
+            optimizer.set_build_flag(TopsInference.KFP16)
+        if config.build_flag == 'TopsInference.KDEFAULT':
+            optimizer.set_build_flag(TopsInference.KDEFAULT)
         engine = optimizer.build(network)
-        return EnflameModel(model_inputs=[input_spec for input_spec in graph.input
-                            if input_spec.name not in initializers],
+        return EnflameModel(model_inputs=[
+            input_spec for input_spec in graph.input
+            if input_spec.name not in initializers
+        ],
                             model_outputs=graph.output,
                             engine=engine,
                             input_formats=config.input_formats)
--- model_compiler/tests/model_compiler/compilers/test_onnx_model_file_to_enflame_model.py
+++ model_compiler/tests/model_compiler/compilers/test_onnx_model_file_to_enflame_model.py
@@ -13,15 +13,20 @@ import model_compiler.compilers.tf_frozen_graph_model_to_onnx_model as frozen_gr
 import model_compiler.compilers.onnx_model_file_to_enflame_model as compiler \
     # pylint: disable=no-name-in-module
 
+
 from model_compiler.compilers.onnx_model_file_to_enflame_model import Config, DataFormat \
     # pylint: disable=no-name-in-module
+
 from model_compiler.models.irs.tf_model import Input, TensorFlowModel
 from model_compiler.models.sources.onnx_model_file import ONNXModelFile
 
 
 def _make_onnx_model():
-    with tf.Graph().as_default(), tf.compat.v1.Session().as_default() as session:
-        input_x = tf.compat.v1.placeholder(dtype=tf.float32, shape=[3, 4], name='x')
+    with tf.Graph().as_default(), tf.compat.v1.Session().as_default(
+    ) as session:
+        input_x = tf.compat.v1.placeholder(dtype=tf.float32,
+                                           shape=[3, 4],
+                                           name='x')
         weight = tf.Variable(initial_value=4.2, dtype=tf.float32)
         output_z = tf.multiply(input_x, weight, name='z')
 
@@ -30,30 +35,53 @@ def _make_onnx_model():
     frozen_graph_model = tf_model_compiler.compile_source(
         source=TensorFlowModel(inputs=[Input(tensor=input_x)],
                                outputs=[output_z],
-                               session=session)
-    )
+                               session=session))
     return frozen_graph_compiler.compile_source(frozen_graph_model)
 
 
 @pytest.mark.dtu_test
 class ConfigTestCase(TestCase):
+
     def test_from_json(self):
-        self.assertEqual(Config.from_json({'input_formats': ['channels_first']}),
-                         Config(input_formats=[DataFormat.CHANNELS_FIRST]))
+        self.assertEqual(
+            Config.from_json({
+                'input_formats': ['channels_first'],
+                'input_shapes': [4, 3, 224, 224],
+                'resource_mode': 'base_resource',
+                'build_flag': 'TopsInference.KFP16'
+            }),
+            Config(input_formats=[DataFormat.CHANNELS_FIRST],
+                   input_shapes=[4, 3, 224, 224],
+                   resource_mode='base_resource',
+                   build_flag='TopsInference.KFP16'))
 
     def test_from_env(self):
-        self.assertEqual(Config.from_env({'INPUT_FORMATS': 'channels_first'}),
-                         Config(input_formats=[DataFormat.CHANNELS_FIRST]))
+        self.assertEqual(
+            Config.from_env({
+                'INPUT_FORMATS': 'channels_first',
+                'INPUT_SHAPES': [4, 3, 224, 224],
+                'RESOURCE_MODE': 'base_resource',
+                'BUILD_FLAG': 'TopsInference.KFP16'
+            }),
+            Config(input_formats=[DataFormat.CHANNELS_FIRST],
+                   input_shapes=[4, 3, 224, 224],
+                   resource_mode='base_resource',
+                   build_flag='TopsInference.KFP16'))
 
 
 @pytest.mark.dtu_test
 class CompileSourceTestCase(TestCase):
+
     def test_compile_with_variables(self):
         with NamedTemporaryFile(suffix='.onnx') as model_file:
             onnx.save_model(_make_onnx_model().model_proto, model_file.name)
 
             config = Config.from_json({'input_formats': ['channels_first']})
-            compiled = compiler.compile_source(source=ONNXModelFile(model_file.name), config=config)
+            compiled = compiler.compile_source(source=ONNXModelFile(
+                model_file.name),
+                                               config=config)
 
-        self.assertEqual([model_input.name for model_input in compiled.model_inputs], ['x:0'])
+        self.assertEqual(
+            [model_input.name for model_input in compiled.model_inputs],
+            ['x:0'])
         self.assertEqual(compiled.input_formats, [DataFormat.CHANNELS_FIRST])
